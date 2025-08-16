be5649da254bd693e24d90349585a17e9c6272da
Mr.Adnan <126695322+adnanTheSmasher@users.noreply.github.com>
2025-08-16 20:12:52 +0500

    Add files via upload

---
+++ MasterMind/MasterMind.py
@@ -0,0 +1,80 @@
+import random
+
+COLORS = ["R", "Y", "B", "G", "W", "O"]
+CODE_LENGTH = 4
+TRIES = 10
+
+def generate_code():
+    code = []
+
+    for _ in range(CODE_LENGTH):
+        color = random.choice(COLORS)
+        code.append(color)
+    
+    return code
+
+def guess_code():
+    while True:
+        guess = input("GUESS: ").upper().strip(" ")
+
+        if len(guess) != CODE_LENGTH:
+            print(f"You must guess {CODE_LENGTH} colors")
+            continue
+        for color in guess:
+            if color not in COLORS:
+                print(f"Invalid Color: {color}")
+                break
+        else:
+            break
+
+    return guess
+
+
+def check_code(guess, real_code):
+    color_count = {}
+    correct_pos = 0
+    incorrect_pos = 0
+
+    for color in real_code:
+        if color not in color_count:
+            color_count[color] = 0
+        color_count[color] += 1
+
+    for guess_color, real_color in zip(guess, real_code):     # For correct Code
+        if guess_color == real_color:
+            correct_pos += 1
+            color_count[guess_color] -= 1
+        
+    for guess_color, real_color in zip(guess, real_color):
+        if guess_color in color_count and color_count[guess_color] > 0:
+            incorrect_pos += 1
+            color_count[guess_color] -= 1
+            
+    return correct_pos, incorrect_pos
+
+
+def game():
+    print(f"Welcome to MasterMind! You have {TRIES} to guess to correct Color.....")
+    print(f"The Valid Colors are: {COLORS}")
+
+    code = generate_code()
+
+    for attempts in range(1, TRIES+1):    
+        guess = guess_code()
+        correct_pos, incorrect_pos = check_code(guess, code)
+
+        if correct_pos == CODE_LENGTH:
+            print(f"Congrats You in {attempts} tries....")
+            break
+
+        print(f"Correct POS: {correct_pos} | Incorrect Pos: {incorrect_pos}")
+
+    else:
+        print("You Ran out of Tries. The Code was: ", *code)
+
+
+if __name__ == "__main__":
+    game()
+
+        
+
